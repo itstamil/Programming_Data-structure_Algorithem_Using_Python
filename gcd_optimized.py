ce3e872b652041b01ee720809c7affe7e3de1fcf
Tamilvalavan Shanmugam <tamilvalavan.shanmugam@infynect.com>
2020-01-28 18:15:44 +0530

    An Optimized code for GCD

---
+++ gcd_optimized.py
@@ -0,0 +1,12 @@
+def gcd(m, n):
+	cf = []
+	for i in range(1,min(m,n)+1):
+		if (m%i) == 0 and (n%i) == 0:
+			cf.append(i)
+	return(cf[-1])
+
+print("Enter Values to fing Greatest common Divider")
+input1 = input("Enter the First Value :")
+input2 = input("Enter the Second Value :")
+GCD = gcd(int(input1), int(input2))
+print('The common Diviser is',GCD)
